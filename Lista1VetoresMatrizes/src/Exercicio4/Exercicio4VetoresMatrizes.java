9e8d804af28ca9c87816642bd3619ca0681bf079
Matheus Prado <ppmatheus01@gmail.com>
2024-05-18 21:44:06 -0300

    Atualizando Lista de Java

--- Lista1VetoresMatrizes/src/Exercicio4/Exercicio4VetoresMatrizes.java
+++ Lista1VetoresMatrizes/src/Exercicio4/Exercicio4VetoresMatrizes.java
@@ -2,6 +2,7 @@ package Exercicio4;
 
 public class Exercicio4VetoresMatrizes {
     public static void main(String[] args) {
+        MediaModaDesvio mmd = new MediaModaDesvio();
         float[][] matriz = new float[][]{
             {1, 8, 5, 7, 6.66f},
             {2, 3, 7, 5, 5},
@@ -9,39 +10,10 @@ public class Exercicio4VetoresMatrizes {
             {4, 0, 8, 7, 5},
             {5, 7, 4, 5, 5.33f}
         };
-        float[][] resultados = new float[4][3];
-
-        for (int col = 1; col < matriz[0].length; col++) {
-            float soma = 0;
-            int count = 0;
-            float moda = 0;
-            int freq = 0;
-            for (int linha = 0; linha < matriz.length; linha++) {
-                soma += matriz[linha][col];
-            }
-            for (int linha = 0; linha < matriz.length; linha++) {
-                freq = 0;
-                for (int i = 0; i < matriz.length; i++) {
-                    if (matriz[linha][col] == matriz[i][col]) {
-                        freq++;
-                    }
-                }
-                if (freq > count) {
-                    count = freq;
-                    moda = matriz[linha][col];
-                }
-            }
-            float media = soma / matriz.length;
-            resultados[col-1][0] = media;
-            resultados[col-1][1] = moda;
-            for (int row = 0; row < matriz.length; row++) {
-                soma += (matriz[row][col] - media) * (matriz[row][col] - media);
-            }
-            float variancia = soma / matriz.length; // variancia
-            resultados[col-1][2] = (float) Math.sqrt(variancia); // desvio padrao
-        }
-        for (int i = 0; i < resultados.length; i++) {
-            System.out.printf("Prova %d: Media = %.2f, Moda = %.2f, Desvio Padrao = %.2f%n", i+1, resultados[i][0], resultados[i][1], resultados[i][2]);
+        mmd.calculaMMD(matriz);
+        
+        for (int i = 0; i < mmd.calculaMMD(matriz).length; i++) {
+            System.out.printf("Prova %d: Media = %.2f | Moda = %.2f | Desvio Padrao = %.2f%n", i+1, mmd.calculaMMD(matriz)[i][0], mmd.calculaMMD(matriz)[i][1], mmd.calculaMMD(matriz)[i][2]);
         }
     }
 }
--- Lista1VetoresMatrizes/src/Exercicio5/Exercicio5VetoresMatrizes.java
+++ Lista1VetoresMatrizes/src/Exercicio5/Exercicio5VetoresMatrizes.java
@@ -2,6 +2,7 @@ package Exercicio5;
 
 public class Exercicio5VetoresMatrizes {
     public static void main(String[] args) {
+        MediaModaDesvio mmd = new MediaModaDesvio();
         float[][] matriz = new float[][]{
             {1, 8, 5, 7, 6.66f},
             {2, 3, 7, 5, 5},
@@ -9,59 +10,22 @@ public class Exercicio5VetoresMatrizes {
             {4, 0, 8, 7, 5},
             {5, 7, 4, 5, 5.33f}
         };
-        float[][] resultados = new float[4][3];
-
-        for (int col = 1; col < matriz[0].length; col++) {
-            float soma = 0;
-            int count = 0;
-            float moda = 0;
-            int freq = 0;
-            for (int linha = 0; linha < matriz.length; linha++) {
-                soma += matriz[linha][col];
-            }
-            for (int linha = 0; linha < matriz.length; linha++) {
-                freq = 0;
-                for (int i = 0; i < matriz.length; i++) {
-                    if (matriz[linha][col] == matriz[i][col]) {
-                        freq++;
-                    }
-                }
-                if (freq > count) {
-                    count = freq;
-                    moda = matriz[linha][col];
-                }
-            }
-            float media = soma / matriz.length;
-            resultados[col-1][0] = media;
-            resultados[col-1][1] = moda;
-            for (int row = 0; row < matriz.length; row++) {
-                soma += (matriz[row][col] - media) * (matriz[row][col] - media);
-            }
-            float variancia = soma / matriz.length; // variancia
-            resultados[col-1][2] = (float) Math.sqrt(variancia); // desvio padrao
-        }
-        for (int c=1; c < resultados[0].length; c++){
-            
-        }
         
-        for (int i = 0; i < resultados.length; i++) {
-            if(i<3){
-                System.out.printf("P%d     | Media: ", i+1);
+        float[][] novaMatriz = mmd.calculaMMD(matriz);
+        System.out.printf("%-6s | %-7s | %-7s | %-14s%n", "|", "Media", "Moda", "Desvio Padrao |");
+        for (int i = 0; i < novaMatriz.length; i++) {
+            if(i < 3){
+                System.out.printf("|P%-5d| ", i+1);
             }else{
-                System.out.printf("PMedia%d| Media: ", i+1);
+                System.out.printf("|PMedia| ");
             }
-            for (int j = 0; j < resultados[i].length; j++) {
+            for (int j = 0; j < novaMatriz[i].length; j++) {
                 if (j > 0) {
                     System.out.printf(" | ");
                 }
-                if (j == 1) {
-                    System.out.printf("Moda: ");
-                } else if (j == 2) {
-                    System.out.printf("Desvio Padrao: ");
-                }
-                System.out.printf("%.2f", resultados[i][j]);
+                System.out.printf(" %-6.2f", novaMatriz[i][j]);
             }
-            System.out.println();
+            System.out.println("       |");
         }
     }
 }
