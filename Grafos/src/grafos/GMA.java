c0314631b1fa81f6a6f1441c849a5e1e86b039a8
mdoisp <ppmatheus01@gmail.com>
2024-06-07 20:59:31 -0300

    Add aula de grafos

---
+++ Grafos/src/grafos/GMA.java
@@ -0,0 +1,26 @@
+package grafos;
+
+public class GMA {
+    Integer matriz[][];
+    Object listaVertices[];
+    boolean grafoDirecional;
+    public GMA(int qntVertices, boolean grafoDirecional){
+        matriz = new Integer [qntVertices][qntVertices];
+        listaVertices = new Object[qntVertices];
+    }
+    public void addVertice(){
+        
+    }
+    public void removeVertice(){
+        
+    }
+    public void addArestaCPeso(Object origem, Object destino, int peso){
+            
+    }
+    public void addArestaSPeso(Object origem, Object destino){
+        
+    }
+    public void imprimirMatrizAdj(){
+        
+    }
+}
